cb22892a2c9a1afe83818c60d58013ce6b1ece0a
Lois <loisgonzalezalonso@gmail.com>
2025-07-09 12:13:52 +0200

    level_2.exercici_2

--- src/Level_2/Exercici_2/MainExercici2.java
+++ src/Level_2/Exercici_2/MainExercici2.java
@@ -5,42 +5,37 @@ import java.util.*;
 public class MainExercici2 {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
         HashSet<Restaurant> myRestaurants = new HashSet<>();
+        List<Restaurant> myRestaurantsOrdered = new ArrayList<>();
+
         System.out.println("Welcome to the ranking restaurant App, let´s rank your 5 favorites restaurants! ");
         while (myRestaurants.size() < 5) {
-            System.out.println("Please enter the name of the new restaurant: ");
-            String name = sc.nextLine();
+            Restaurant r = new Restaurant();
+            r.setName();
+            r.setPoints();
 
-            int points = 0;
-            boolean flag = true;
-            while(flag) {
-                System.out.println("Please enter the point of the new restaurant: ");
-                try {
-                    points = sc.nextInt();
-                    flag = false;
-                } catch (InputMismatchException e) {
-                    System.out.println("Please enter a valid number: ");
-                }
-                sc.nextLine();
-            }
-            Restaurant r = new Restaurant(name, points);
             if (myRestaurants.isEmpty()) {
                 myRestaurants.add(r);
-            } else {
+            }
+            else {
                 if (r.checkData(myRestaurants)) {
                     myRestaurants.add(r);
+                    myRestaurantsOrdered.add(r);
 
                 } else {
                     System.out.println("You cannot add a restaurant with the same name and the same points ");
+                    System.out.println("The restaurant '" + r.getName() + "' was not added");
                 }
             }
             System.out.println("Right now you have in your set: ");
             for (Restaurant values : myRestaurants) {
                 System.out.println("[Restaurant Name -> " + values.getName() + "] [Points -> " + values.getPoints() + "]");
             }
-            List<Restaurant> myRestaurantsOrdered = new ArrayList<>(myRestaurants);
-            Collection.sort(myRestaurantsOrdered);
+        }
+        Collections.sort(myRestaurantsOrdered);
+        System.out.println("This are the restaurants ordered in descending order");
+        for (Restaurant values : myRestaurantsOrdered){
+            System.out.println("[Restaurant Name -> " + values.getName() + "] [Points -> " + values.getPoints() + "]");
         }
     }
 }
--- src/Level_2/Exercici_2/Restaurant.java
+++ src/Level_2/Exercici_2/Restaurant.java
@@ -1,15 +1,13 @@
 package Level_2.Exercici_2;
 
-import java.util.HashSet;
+import java.util.*;
 
 
-public class Restaurant {
+public class Restaurant implements Comparable<Restaurant> {
     private String name;
     private int points;
 
-    public Restaurant(String name, int points) {
-        this.name = name;
-        this.points = points;
+    public Restaurant() {
     }
 
     public String getName() {
@@ -20,6 +18,33 @@ public class Restaurant {
         return points;
     }
 
+    public void setName(){
+        Scanner scName = new Scanner(System.in);
+        System.out.println("Please enter the name of the new restaurant: ");
+        this.name = scName.nextLine();
+    }
+
+    public void setPoints() {
+        Scanner scPoints = new Scanner(System.in);
+        boolean validInput = false;
+
+        System.out.println("Please enter a score between 1 and 10 for the '" + this.name + "' restaurant");
+
+        while (!validInput) {
+            if (!scPoints.hasNextInt()) {
+                System.out.println("This value is not accepted as a valid score, it has to be between 1 and 10, please, try again");
+                scPoints.next();
+                continue;
+            }
+            this.points = scPoints.nextInt();
+            if (!(this.points <= 10 && this.points >= 1)){
+                System.out.println("This value is not accepted as a valid score, it has to be between 1 and 10, please, try again");
+                continue;
+            }
+            validInput = true;
+            System.out.println("The score you choosed for '" + this.name + "' restaurant is " + this.points);
+            }
+    }
 
     public boolean checkData(HashSet<Restaurant> myRestaurants) {
         for (Restaurant r : myRestaurants) {
@@ -29,5 +54,13 @@ public class Restaurant {
         }
         return true;
     }
+    @Override
+    public int compareTo(Restaurant restaurantPoints) {
+         return Integer.compare(restaurantPoints.points, this.points);
+    }
 }
+
+
+
+
 
